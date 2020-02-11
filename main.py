5e7ed3c502e13b8c420ab5de35a5b40bd38e29bd
Divyansh11000 <divyansh.24888@gmail.com>
2020-02-11 20:05:23 +0530

    0.0.2

--- main.py
+++ main.py
@@ -2,3 +2,21 @@ import pygame
 
 #initialise pygame
 pygame.init()
+
+#creating the screen
+screen = pygame.display.set_mode((800, 600))
+
+
+#Title and icon
+pygame.display.set_caption("The Battle of the Morannon")
+icon =pygame.image.load('onering.png')
+pygame.display.set_icon(icon)
+#Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running  = False
+    #background color
+    screen.fill((255, 0, 50))
+    pygame.display.update()

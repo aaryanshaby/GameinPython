7c5b4f6c879743805b5db0c412260baf38b9cff1
aaryanshaby <aaryansingh0612@gmail.com>
2020-02-18 18:41:08 +0530

    Beta 1.0.5 Score added

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 screen = (1000, 800)
 background_color = bluish hue
-Global_Variables = counter, cr1, cr2, cr3, cr4, cr5, cr6, cr7, cr8, cr9, cr10, cr11, cr12, time_score_change,score_list, score_value, time_score, time_score_change, rounding
-Global_Variables_ for_player =  player1X, player1Y, player2X, player2Y, playerX, playerY, player1X_change, player1Y_change, playerspeed
-Global_Variables_for_enemy =
+Global_Variables = counter, cr1, cr2, cr3, cr4, cr5, cr6, cr7, cr8, cr9, cr10, cr11, cr12, time_score_change,score_list, score_value, time_score, time_score_change, rounding,a,b,c, cute, textA, textX, textY, textZ, font
+Global_Variables_ for_player =  player1img, player2img,player1X, player1Y, player2X, player2Y, playerX, playerY, player1X_change, player1Y_change, playerspeed
+Global_Variables_for_enemy = enemyfor1img,enemyfor2img, enemyfor3img, enemyfor4img, enemyfor5img, enemyfor1X,enemyfor2X, enemyfor3X, enemyfor11X, enemyfor21X,enemyfor1Y, enemyfor2Y, enemyfor3Y, enemyfor11Y, enemyfor21Y
+Global_Variables_for_enemy_speeds = enemyfor1X_change, enemyfor1Y_change, enemyspeed, enemyspeed1, enemyspeed2
+Global_Variables_for_enemy_fixed = fixedobstacle1img, fixedobstacle2img, fixedobstacle3img, fixedobstacle4img, fixedobstacle01img, fixedobstacle02img, fixedobstacle1X, fixedobstacle1Y, fixedobstacle2X, fixedobstacle2Y, fixedobstacle3X, fixedobstacle3Y, fixedobstacle4X, fixedobstacle4Y, fixedobstacle01X, fixedobstacle01Y, fixedobstacle02X, fixedobstacle02Y
+
+Global_functions = show_round, show_score, fbasestation, player1, enemyfor1, enemyfor2, enemyfor3, enemyfor11, enemyfor21,  fixedobstacle1,  fixedobstacle2,  fixedobstacle3,  fixedobstacle4,  fixedobstacle01,  fixedobstacle02, isCollision, isCollisiono, resetgame, gameend
